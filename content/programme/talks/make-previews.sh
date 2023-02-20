08055096c04a79f0da344cb9d0e5f16bffeec3aa
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2023-02-20 13:39:36 +0100

    Don't crash when there are no PDFs

--- content/programme/talks/make-previews.sh
+++ content/programme/talks/make-previews.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# https://www.endpointdev.com/blog/2016/12/bash-loop-wildcards-nullglob-failglob/
+shopt -s nullglob
 for i in *.pdf; do
     pngname=$(basename $i .pdf).png
     echo $i $pngname
